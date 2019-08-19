336d9ea27fda13f1b589b02e7ab40804e3310f75
jjvargass <jotavargas89@gmail.com>
2019-08-19 17:39:40 -0500

    primer tarea con job dsl

---
+++ jenkins-python-pipelineJob-01.groovy
@@ -0,0 +1,25 @@
+project_name = "jenkins-python-pipelineJob-01"
+repo = "git@github.com:jjvargass/test-jenkins-01.git"
+repo_name = "repo"
+
+
+pipelineJob(project_name){
+  definition{
+      triggers{
+          scm('H/1 * * * *')
+      }
+
+      cpsScm {
+          scm {
+              git {
+                  remote {
+                      name{repo_name}
+                      url(repo)
+                  }
+                }
+              scriptPath{""}
+          }
+      }
+
+  }
+}
